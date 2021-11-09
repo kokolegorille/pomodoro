f1497e2ee74b568f91d60f889145acee0e6103e3
kokolegorille <koko.le.gorille@gmail.com>
2021-11-09 13:31:49 +0100

    Configure dev server

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import "./css/app.scss";
+
+import React from "react";
+import { render } from "react-dom";
+
+import App from "./js/App";
+
+const app = document.getElementById("app");
+render(
+    <App />, 
+    app
+);
